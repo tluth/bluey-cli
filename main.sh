99ae31a4db86ad38466f6e456556cfcdf4cdc851
tluth <tim.lutherborrow@gmail.com>
2024-01-12 23:17:39 +0100

    get wrking

--- main.sh
+++ main.sh
@@ -1,27 +1,29 @@
 
 #!/bin/bash
 
-device_name=$#;
-all_instances=()
+device_name=$1;
 
-while IFS= read -r line; do
-    all_instances+=( "$line" )
-    case "$line" in 
-    *$device_name*)
-        # echo $line
+# Make sure bluetooth adapter is discoverable 
+bluetoothctl discoverable on
 
-        ;;
-    esac
+bluetoothctl paired-devices | cut -f2 -d' '|
+while read -r uuid
+do
+    info=`bluetoothctl info $uuid`
+    echo $info
+    if echo "$info" | grep -q "Connected: no"; then
+       mac_address=$( echo $info | grep -oP "([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}")
+       echo $mac_address
+       bluetoothctl remove $mac_address
+    fi
+done
 
-done < <( bluetoothctl devices )
-
-printf '%s\n' "${all_instances[@]}"
-
-
-# get_mac_address () {
-#   return $1 | grep -oP "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$"
-# }
-
-
-echo "Device CD:49:CC:9B:50:FD Modern Mobile Mouse" | grep -oP "([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}"
-
+bluetoothctl devices | cut -f2 -d' '|
+while read -r uuid
+do
+    info=`bluetoothctl info $uuid`
+    if echo "$info" | grep -q "Name: $device_name"; then 
+        bluetoothctl pair "$info" | grep "Name" | grep -v "Name: " 
+        bluetoothctl connect "$info" | grep "Name" | grep -v "Name: " 
+    fi
+done
